e6769049aa45e7c69fe73680eba21758921ac848
GergelyIstvanRobert <skiridershop@gmail.com>
2019-11-20 20:26:48 +0200

    studied methods

--- src/main/java/org/fasttrackit/Vehicle.java
+++ src/main/java/org/fasttrackit/Vehicle.java
@@ -9,4 +9,22 @@ public class Vehicle {
     double fuelLevel;
     double traveledDistance;
     boolean running;
+
+    public double accelerate(double speed,double durationInHours) {
+        System.out.println(name + " is accelerating whit " + speed + "for" + durationInHours + "h.");
+
+        double distance = speed * durationInHours;
+
+        traveledDistance = traveledDistance + distance;
+//      same result as the statmement above
+//        traveledDistance += distance
+        System.out.println("Total traveled distnace: " + traveledDistance);
+
+        double usedFuel = distance * mileage / 100;
+        System.out.println("Used fuel: " + usedFuel);
+        fuelLevel -= usedFuel;
+        System.out.println("Remaining fuel level: " + fuelLevel);
+
+        return distance;
+    }
 }
