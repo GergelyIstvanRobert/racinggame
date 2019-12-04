39d3ee4a44cf2eef3d42fe7e6b64223d5304b63a
GergelyIstvanRobert <skiridershop@gmail.com>
2019-12-04 21:16:31 +0200

    studied arrays and classic for loop

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -15,108 +15,108 @@ public class App
 
         Game game = new Game();
         game.start();
-        //method implementation taken from object class,not from variable class
-
-        Vehicle joker = new JokerVehicle();
-        joker.setName("Joker");
-        joker.setFuelLevel(80);
-
-        joker.accelerate(60,1);
-        System.out.println("Joker distance : " + joker.getTraveledDistance());
-
-        //variable class determines what methods can be invoked type casting
-        ((JokerVehicle) joker).fly();
-
-        Car carWhitdefaultEngine = new Car();
-
-        Engine carEngine = new Engine();
-        carEngine.manufacturer = "Renault";
-        carEngine.capacity = 1.5;
-
-       Car carReference = new Car(carEngine);
-       carReference.setName("Dacia");
-       carReference.setColor("Red");
-       carReference.setMileage(9.8);
-       carReference.setFuelLevel(60);
-       carReference.setMaxSpeed(200);
-       carReference.setRunning(false);
-       carReference.doorCount = 2;
-
-
-
-       carReference.engine = carEngine;
-
-        System.out.println("Engine details...");
-        System.out.println(carReference.engine.manufacturer);
-        System.out.println(carReference.engine.capacity);
-
-        double accelerationDistance = carReference.accelerate(60,1);
-        System.out.println("Acceleration distance: " + accelerationDistance);
-
-        Mechanic mechanic = new  Mechanic ();
-        mechanic.repairVehicle(carReference);
-
-        System.out.println("Total traveled distance after repair : " + carReference.getTraveledDistance());
-
-        Engine car2Engine = new Engine();
-        car2Engine.manufacturer = "Bavaria Motors";
-        car2Engine.capacity = 12.2;
-
-
-       Car car2 = new Car(car2Engine);
-       car2.setName("BMW");
-       car2.setMileage(14);
-       car2.setColor(null);
-
-       //concatenation
-        System.out.println(carReference);
-        System.out.println("First car name: "+ carReference.getName());
-        System.out.println(carReference.getColor());
-        System.out.println(carReference.getMileage());
-        System.out.println(carReference.getFuelLevel());
-        System.out.println(carReference.doorCount);
-        System.out.println(carReference.getMaxSpeed());
-        System.out.println(carReference.isRunning());
-        System.out.println(carReference.getTraveledDistance());
-
-        System.out.println("Second car name: "+ car2.getName());
-        System.out.println(car2.getMileage());
-        System.out.println(car2.getFuelLevel());
-        System.out.println(car2.doorCount);
-        System.out.println(car2.getMaxSpeed());
-        System.out.println(car2.isRunning());
-        System.out.println(car2.getTraveledDistance());
-
-         car2.setName("VW");
-        System.out.println(car2.getName());
-
-        Car car3 = car2;
-        car3.setName("Audi");
-
-        System.out.println("car2 name: "+ car2.getName());
-        System.out.println("car3 name: "+ car3.getName());
-
-        System.out.println("Modulo example: ");
-        System.out.println(4 % 2);
-        System.out.println(4 % 3);
-        System.out.println(4 % 2 == 0);
-
-//    example for NullPointerEXception
-//     car car4 = null;
-//     System.out.println("car4.name")
-
-        System.out.println("studynimg class variables (static variables)...");
-        Vehicle vehicle1 = new Vehicle();
-        vehicle1.totalCount = 10 ;
-
-         Vehicle vehicle2 = new Vehicle();
-         vehicle2.totalCount = 20;
-
-          Vehicle.totalCount = 30;
-
-        System.out.println("Total vehicle count from vehicle1: " + vehicle1.totalCount);
-        System.out.println("Total vehicle count from vehicle2: " + vehicle2.totalCount);
-        System.out.println("Total vehicle count from Vehicle class: " + vehicle1.totalCount);
+//        //method implementation taken from object class,not from variable class
+//
+//        Vehicle joker = new JokerVehicle();
+//        joker.setName("Joker");
+//        joker.setFuelLevel(80);
+//
+//        joker.accelerate(60,1);
+//        System.out.println("Joker distance : " + joker.getTraveledDistance());
+//
+//        //variable class determines what methods can be invoked type casting
+//        ((JokerVehicle) joker).fly();
+//
+//        Car carWhitdefaultEngine = new Car();
+//
+//        Engine carEngine = new Engine();
+//        carEngine.manufacturer = "Renault";
+//        carEngine.capacity = 1.5;
+//
+//       Car carReference = new Car(carEngine);
+//       carReference.setName("Dacia");
+//       carReference.setColor("Red");
+//       carReference.setMileage(9.8);
+//       carReference.setFuelLevel(60);
+//       carReference.setMaxSpeed(200);
+//       carReference.setRunning(false);
+//       carReference.doorCount = 2;
+//
+//
+//
+//       carReference.engine = carEngine;
+//
+//        System.out.println("Engine details...");
+//        System.out.println(carReference.engine.manufacturer);
+//        System.out.println(carReference.engine.capacity);
+//
+//        double accelerationDistance = carReference.accelerate(60,1);
+//        System.out.println("Acceleration distance: " + accelerationDistance);
+//
+//        Mechanic mechanic = new  Mechanic ();
+//        mechanic.repairVehicle(carReference);
+//
+//        System.out.println("Total traveled distance after repair : " + carReference.getTraveledDistance());
+//
+//        Engine car2Engine = new Engine();
+//        car2Engine.manufacturer = "Bavaria Motors";
+//        car2Engine.capacity = 12.2;
+//
+//
+//       Car car2 = new Car(car2Engine);
+//       car2.setName("BMW");
+//       car2.setMileage(14);
+//       car2.setColor(null);
+//
+//       //concatenation
+//        System.out.println(carReference);
+//        System.out.println("First car name: "+ carReference.getName());
+//        System.out.println(carReference.getColor());
+//        System.out.println(carReference.getMileage());
+//        System.out.println(carReference.getFuelLevel());
+//        System.out.println(carReference.doorCount);
+//        System.out.println(carReference.getMaxSpeed());
+//        System.out.println(carReference.isRunning());
+//        System.out.println(carReference.getTraveledDistance());
+//
+//        System.out.println("Second car name: "+ car2.getName());
+//        System.out.println(car2.getMileage());
+//        System.out.println(car2.getFuelLevel());
+//        System.out.println(car2.doorCount);
+//        System.out.println(car2.getMaxSpeed());
+//        System.out.println(car2.isRunning());
+//        System.out.println(car2.getTraveledDistance());
+//
+//         car2.setName("VW");
+//        System.out.println(car2.getName());
+//
+//        Car car3 = car2;
+//        car3.setName("Audi");
+//
+//        System.out.println("car2 name: "+ car2.getName());
+//        System.out.println("car3 name: "+ car3.getName());
+//
+//        System.out.println("Modulo example: ");
+//        System.out.println(4 % 2);
+//        System.out.println(4 % 3);
+//        System.out.println(4 % 2 == 0);
+//
+////    example for NullPointerEXception
+////     car car4 = null;
+////     System.out.println("car4.name")
+//
+//        System.out.println("studynimg class variables (static variables)...");
+//        Vehicle vehicle1 = new Vehicle();
+//        vehicle1.totalCount = 10 ;
+//
+//         Vehicle vehicle2 = new Vehicle();
+//         vehicle2.totalCount = 20;
+//
+//          Vehicle.totalCount = 30;
+//
+//        System.out.println("Total vehicle count from vehicle1: " + vehicle1.totalCount);
+//        System.out.println("Total vehicle count from vehicle2: " + vehicle2.totalCount);
+//        System.out.println("Total vehicle count from Vehicle class: " + vehicle1.totalCount);
 
 
 
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -6,7 +6,12 @@ import java.util.Scanner;
 
 public class Game {
 
+    private Track [] tracks = new  Track[3];
+
+
     public void start() {
+        initializeTrakcs();
+        displayTracks();
 
         System.out.println("Starting game...");
         String vehicleName = getVehicleNameFromUser();
@@ -15,6 +20,34 @@ public class Game {
         System.out.println("Number of player:" + playerCount);
 
     }
+    private void  initializeTrakcs () {
+        Track track1 = new Track();
+        track1.setName("Silverstone");
+        track1.setLength(4.2);
+
+        tracks[0] = track1;
+
+        Track track2 = new Track();
+        track2.setName("Transylvania");
+        track2.setLength(400.2);
+
+        tracks[1] = track2;
+    }
+    private  void displayTracks () {
+        System.out.println("Avaliable traks : ");
+        //classic for loop
+        for (int i = 0; i < tracks.length; i++){
+            if (tracks[i] != null) {
+                int j = i+1;
+                System.out.println(j+"." + tracks[i].getName() + "-" + tracks[i].getLength() + "km");
+                //(i+1) + "." +(tracks[i].getName() + "-" + tracks [i].getLenght() + " km"
+            }
+        }
+    }
+
+
+
+
 
     private String getVehicleNameFromUser() {
         System.out.println("Please enter vehicle name.");
